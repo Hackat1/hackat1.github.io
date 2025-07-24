6e39e302fd07ccb10c8089853cf59cee5eb56461
Christopher Hackathorn <chris.hack01@gmail.com>
2025-07-23 21:43:12 -0700

    Create team.js

---
+++ js/team.js
@@ -0,0 +1,18 @@
+const team = [
+  {
+    name: "Deidre Buchan",
+    title: "Bookkeeper",
+    photo: "assets/deidre.jpg",
+    bio: `Deidre Buchan has a strong foundation in mathematics, having earned a degree in Mathematics with a minor in Actuarial Science from the University of Washington. She holds an Intuit Bookkeeping Certification as well as Level 1 & 2 QuickBooks certifications.
+
+Deidre is equipped with industry leading tools and knowledge to help businesses maintain accurate financial records and make informed decisions. She has a passion for supporting local business growth as small businesses are the backbone of our communities.`
+  },
+  {
+    name: "Neo Vincent",
+    title: "Financial Planning & Strategy",
+    photo: "assets/neo.jpg",
+    bio: `Neo Vincent has over 15 years of Financial Planning and Strategy experience across multiple industries focusing on small to midsize companies. They earned an MBA from University of Washington. Key areas of focus include annual budgeting, forecast cycles, ROI analysis, and cash flow strategy.
+
+Neo has a passion for distilling large amounts of financial data into actionable insight to drive profitability and long-term growth for businesses.`
+  }
+];
